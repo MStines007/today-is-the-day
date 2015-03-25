7fde047fbae9e3b32192bb857830f578500667e2
MStines007 <mstines007@gmail.com>
2015-03-25 15:27:19 -0400

    completed wiki_items model

--- app/models/user.rb
+++
@@ -1,5 +0,0 @@
-class User < ActiveRecord::Base
-
-	validates_presence_of :name
-
-end
--- db/migrate/20150323150428_create_news.rb
+++
@@ -1,7 +0,0 @@
-class CreateNews < ActiveRecord::Migration
-  def change
-    create_table :news do |t|
-      t.datetime :date
-    end
-  end
-end
