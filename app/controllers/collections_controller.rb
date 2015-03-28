feb11c642d7c6d49b9231aa25585705c097db9cb
Skylar Panuska <skylar.panuska@gmail.com>
2015-03-28 12:13:30 -0400

    creates instances of news items

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -8,7 +8,12 @@ class CollectionsController < ApplicationController
 	def create
 		@collection = Collection.create(:date => params[:collection][:date])
 		@collection.wiki_items.build(:date => params[:collection][:date])
+		#@collection.giphy_items.build(:date => params[:collection][:date])
+
+		@collection.news_items = NewsItem.createFromDate(params[:collection][:date])
+
 		@collection.save
+		# binding.pry
 		redirect_to collection_path(@collection.id)
 	end
 
