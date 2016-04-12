8adfba8776b384b109600b1786c014da36c2eb98
Skylar Panuska <skylar.panuska@gmail.com>
2016-04-12 15:17:22 -0400

    abstracts the list of words to filter out of nyt searches, adds some words to the list

--- app/models/news_item.rb
+++ app/models/news_item.rb
@@ -16,13 +16,18 @@ class NewsItem < ActiveRecord::Base
   end
 
   def self.api_call(date)
-    url = "http://api.nytimes.com/svc/search/v2/articlesearch.json?fq=section_name.contains:Front&begin_date=#{self.get_date_string(date)}&end_date=#{get_date_string(date)}&-war&-death&-kill&-die&-rape&api-key=#{ENV['NYTIMES_KEY']}"
+    url = "http://api.nytimes.com/svc/search/v2/articlesearch.json?fq=section_name.contains:Front&begin_date=#{self.get_date_string(date)}&end_date=#{get_date_string(date)}#{filtered_words}&api-key=#{ENV['NYTIMES_KEY']}"
     response_hash = JSON.load(open(url))
   end
 
 # ----- HELPERS
   def self.get_date_string(date)
     date.to_s.gsub("-","")[0..7]
+  end
+
+  def self.filtered_words
+    words = %w(war death kill kills killed die died rape raped rapes killer killing dies kidnap kidnaps kidnapped)
+    list = "&-" + words.join("&-")
   end
 
 end
